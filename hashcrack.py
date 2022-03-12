7be2b0d12a8a3dd9f973aafad9e973ad1f9b0b55
sanglantes <101125878+sanglantes@users.noreply.github.com>
2022-03-12 17:43:40 +0100

    Update hashcrack.py

--- hashcrack.py
+++ hashcrack.py
@@ -1,6 +1,8 @@
 import argparse
-import codecs
+from sty import bg
 from hashlib import *
+
+
 parser = argparse.ArgumentParser(description=f'''HashCrack is a Python-based hash cracker with support for salts.
 Available hash functions are: {'MD5, SHA1, SHA224, SHA256, SHA384, SHA512, SHA3_224, SHA3_256, SHA3_384, SHA3_512, BLAKE2B, BLAKE2S'.lower()}''')
 parser.add_argument('-H', nargs='?', metavar='hash', required=True, dest='hash', help='string to be cracked')
@@ -19,7 +21,6 @@ unparsed = f.read()
 unparsed = unparsed.replace('\n', ' ').split(' ')
 f.close()
 i = 0
-
 with open(f'{dictionary}', 'rb') as f:
     for line in f.readlines():
         pw = line.strip()
@@ -29,6 +30,9 @@ with open(f'{dictionary}', 'rb') as f:
         print(f'{unparsed[i]} {digest}')
         i = i + 1
         if attack_hash == digest:
-            print(f'\033[1;32;40mCRACKED! {attack_hash} is {unparsed[i - 1]}')
+            print(f'{bg.green}CRACKED! {attack_hash} is {unparsed[i - 1]}{bg.green}')
+            print(f'{bg.black}')
             break
-print(f'\033[1;31;40mCould not crack the hash.')
+if not attack_hash == digest:
+    print(f'{bg.red}Could not crack the hash.{bg.red}')
+    print(f'{bg.black}')
